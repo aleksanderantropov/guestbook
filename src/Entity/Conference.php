9a02cc497830e045b21ff1499172051b0a188edb
aleksanderantropov <aleksander.antropov@gmail.com>
2021-03-05 03:03:21 +0300

    Add API

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,16 +2,25 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
  * @UniqueEntity("slug")
+ * 
+ * @ApiResource(
+ *  collectionOperations={"get"={"normalization_context"={"groups"="conference:list"}}},
+ *  itemOperations={"get"={"normalization_context"={"groups"="conference:item"}}},
+ *  order={"year"="DESC", "city"="ASC"},
+ *  paginationEnabled=false
+ * )
  */
 class Conference
 {
@@ -20,16 +29,19 @@ class Conference
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $city;
 
     /**
      * @ORM\Column(type="string", length=4)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $year;
 
     /**
@@ -45,6 +57,7 @@ class Conference
     /**
      * @ORM\Column(type="string", length=255, unique=true)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $slug;
 
     public function __construct()
